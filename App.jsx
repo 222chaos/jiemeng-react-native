33f417d76d6b9eb70877fda6c185b681617075a9
2chaos <1585767724@qq.com>
2023-11-28 23:54:32 +0800

    iframe嵌入

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {WebView} from 'react-native-webview';
+
+const NextJSWebView = () => {
+  return (
+    <WebView
+      source={{uri: 'https://jiemeng.chenshuai.dev'}}
+      style={{flex: 1}}
+    />
+  );
+};
+
+export default NextJSWebView;
