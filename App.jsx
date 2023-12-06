2e993cfea1d5c6b4fa0bd1016b2430ccd30dbc00
2chaos <1585767724@qq.com>
2023-12-06 22:39:18 +0800

    在.xml添加位置权限

--- App.jsx
+++ App.jsx
@@ -1,8 +1,29 @@
-import React from 'react';
-import {SafeAreaView, StyleSheet} from 'react-native';
+import React, {useEffect} from 'react';
+import {SafeAreaView, StyleSheet, Platform} from 'react-native';
 import {WebView} from 'react-native-webview';
+import {request, PERMISSIONS, check, RESULTS} from 'react-native-permissions';
 
 function App() {
+  useEffect(() => {
+    requestLocationPermission();
+  }, []);
+
+  const requestLocationPermission = async () => {
+    let permission = PERMISSIONS.IOS.LOCATION_WHEN_IN_USE;
+    if (Platform.OS === 'android') {
+      permission = PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION;
+    }
+
+    const status = await check(permission);
+
+    if (status === RESULTS.DENIED) {
+      const result = await request(permission);
+      console.log('Permission request result:', result);
+    } else {
+      console.log('Permission already granted or blocked:', status);
+    }
+  };
+
   return (
     <SafeAreaView style={styles.container}>
       <WebView
